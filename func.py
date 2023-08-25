02a24bc8ccfabc2193ea38c42db576845d5a56ba
Pablo Batista <pabloreis2001@gmail.com>
2023-08-25 18:22:51 -0300

    FINISHED

--- func.py
+++ func.py
@@ -2,25 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import networkx as nx
-import matplotlib.pyplot as plt
-import pandas as pd
-
-def criar_grafo(ano, partidos=None):
-    # Leitura do arquivo de políticos
-    politicos_file = f'datasets/politicians{ano}.txt'
-    politicos_df = pd.read_csv(politicos_file, sep='\t', header=None, names=['nome', 'partido', 'votacoes'])
-
-    # Filtrar políticos por partido, se necessário
-    if partidos:
-        politicos_df = politicos_df[politicos_df['partido'].isin(partidos)]
+def criar_grafo(ano):
+    # Leitura do arquivo de votações com delimitador ponto e vírgula
+    votacoes_file = f'datasets/graph{ano}.txt'
+    votacoes_df = pd.read_csv(votacoes_file, sep=';', header=None, names=['deputado1', 'deputado2', 'votacoes_iguais'])
 
     # Criação do grafo
     G = nx.Graph()
-
-    # Leitura do arquivo de votações
-    votacoes_file = f'datasets/graph{ano}.txt'
-    votacoes_df = pd.read_csv(votacoes_file, sep='\t', header=None, names=['deputado1', 'deputado2', 'votacoes_iguais'])
 
     # Adicionar arestas ao grafo com peso igual ao número de votações iguais
     for _, row in votacoes_df.iterrows():
@@ -28,37 +16,50 @@ def criar_grafo(ano, partidos=None):
         deputado2 = row['deputado2']
         votacoes_iguais = row['votacoes_iguais']
         G.add_edge(deputado1, deputado2, weight=votacoes_iguais, ano=ano)
-        
-    print("GRAFO CONSTRUIDO ### FILTRAGEM APLICADA")
-    
+
+    print(f"GRAFO CONSTRUIDO: Nós={len(G.nodes())}, Arestas={len(G.edges())}")
+
     return G  # Retorna o grafo
+
+def filtrar_grafo_por_partidos(grafo, ano, partidos=None):
+    if partidos is None:
+        # Se nenhum partido for especificado, retornar o grafo original sem filtro
+        return grafo
+
+    # Leitura do arquivo de políticos
+    politicos_file = f'datasets/politicians{ano}.txt'
+    politicos_df = pd.read_csv(politicos_file, sep=';', header=None, names=['nome', 'partido', 'votacoes'])
+
+    # Filtrar políticos por partido
+    politicos_filtrados = politicos_df[politicos_df['partido'].isin(partidos)]
+    deputados_filtrados = politicos_filtrados['nome'].tolist()
+
+    # Criar um novo grafo filtrado com base nos deputados do partido
+    G_filtrado = grafo.subgraph(deputados_filtrados).copy()
+
+    print(f"GRAFO FILTRADO POR PARTIDOS ({', '.join(partidos)}): Nós={len(G_filtrado.nodes())}, Arestas={len(G_filtrado.edges())}")
+
+    return G_filtrado
+
 
 # Função para criar e retornar um novo grafo normalizado a partir do grafo G
 def criar_grafo_normalizado(grafo):
     # Cria uma cópia do grafo original para não modificá-lo
     grafo_normalizado = grafo.copy()
     
-    # Lista de arestas a serem removidas
-    arestas_remover = []
-    
-    # Normalizar os pesos das arestas do grafo normalizado
+    # Iterar sobre as arestas do grafo normalizado
     for u, v, w in grafo_normalizado.edges(data=True):
-        # Obter o número mínimo de votações entre os deputados u e v
-        min_votes = min(w['weight'], grafo_normalizado[v][u]['weight'])  # Ajustado para considerar ambas as direções
-        
         # Normalizar o peso da aresta com base na fórmula fornecida
-        if min_votes > 0:
-            grafo_normalizado[u][v]['weight'] = w['weight'] / min_votes
-        else:
-            # Marcar a aresta para remoção se não houver votações em comum
-            arestas_remover.append((u, v))
+        grafo_normalizado[u][v]['weight'] = w['weight'] / min(grafo.degree(u), grafo.degree(v))
+            
+    # Imprimir o número de nós e arestas no grafo normalizado
+    num_nos = len(grafo_normalizado.nodes())
+    num_arestas = len(grafo_normalizado.edges())
+    print(f"GRAFO NORMALIZADO ### Nós={num_nos}, Arestas={num_arestas}")
     
-    # Remover arestas marcadas para remoção
-    for u, v in arestas_remover:
-        grafo_normalizado.remove_edge(u, v)
-    
-    print("GRAFO NORMALIZADO ###")
     return grafo_normalizado
+
+
 
 def criar_grafo_threshold(grafo_normalizado, threshold):
     # Cria uma cópia do grafo normalizado original para não modificá-lo
@@ -66,6 +67,7 @@ def criar_grafo_threshold(grafo_normalizado, threshold):
     
     # Lista de arestas a serem removidas
     arestas_remover = []
+    
     
     # Itera sobre as arestas do grafo normalizado com threshold
     for u, v, w in grafo_com_threshold.edges(data=True):
@@ -77,6 +79,16 @@ def criar_grafo_threshold(grafo_normalizado, threshold):
     for u, v in arestas_remover:
         grafo_com_threshold.remove_edge(u, v)
         
-    print("GRAFO NORMALIZADO com THRESHOLD###")
+    # Imprimir o número de nós e arestas no grafo com threshold
+    num_nos = len(grafo_com_threshold.nodes())
+    num_arestas = len(grafo_com_threshold.edges())
+    print(f"GRAFO NORMALIZADO COM THRESHOLD ### Nós={num_nos}, Arestas={num_arestas}")
     
     return grafo_com_threshold
+
+
+def inverter_pesos(grafo):
+    for u, v, w in grafo.edges(data=True):
+        novo_peso = 1 - w['weight']
+        grafo[u][v]['weight'] = novo_peso
+    print("INVERSÃO DE PESOS CONCLUÍDA !")
--- main.py
+++ main.py
@@ -1,27 +1,76 @@
 from func import *
 import networkx as nx
 import matplotlib.pyplot as plt
+import seaborn as sns
 
+ano = 2023
+partidos_filtrados = ["PT", "PSOL"]
+threshold = 0.9
 
-ano = 2022
-partidos = ["PT", "PL", "MDB"]
-threshold = 0.5
-
-G = criar_grafo(ano, partidos)
-grafo_normalizado = criar_grafo_normalizado(G)
+G_sem_filtro = criar_grafo(ano)
+G_filtrado = filtrar_grafo_por_partidos(G_sem_filtro, ano, partidos_filtrados)
+grafo_normalizado = criar_grafo_normalizado(G_filtrado)
 grafo_threshold = criar_grafo_threshold(grafo_normalizado, threshold)
+inverter_pesos(grafo_threshold)
 
 
+# Calcular a Betweenness Centrality
+betweenness_centrality = nx.betweenness_centrality(grafo_threshold)
 
-# Crie um layout para o grafo para posicionar os nós
-layout = nx.spring_layout(grafo_threshold)
+# Converter o dicionário de centralidades em listas separadas para o gráfico
+nomes_deputados = list(betweenness_centrality.keys())
+centralidade_valores = list(betweenness_centrality.values())
 
-# Extraia os pesos das arestas para determinar a espessura das arestas no gráfico
-edge_weights = [grafo_threshold[u][v]['weight'] for u, v in grafo_threshold.edges()]
+# Criar o gráfico de barras
+plt.figure(figsize=(12, 6))
+plt.barh(nomes_deputados, centralidade_valores, color='skyblue')
+plt.xlabel('Medida de Centralidade')
+plt.ylabel('Deputados')
+plt.title('Betweenness Centrality dos Deputados')
 
-# Crie o plot do grafo
-plt.figure(figsize=(10, 10))  # Ajuste o tamanho da figura conforme necessário
-nx.draw(grafo_threshold, pos=layout, with_labels=True, node_size=50, font_size=8, width=edge_weights, edge_cmap=plt.cm.Blues)
+# Salvar o gráfico em um arquivo de imagem (opcional)
+plt.savefig('betweenness_centrality.png')
 
-# Exiba o gráfico
+# Exibir o gráfico
+plt.show()
+
+#--------------------------------------
+
+# Calcular a matriz de adjacência do grafo normalizado
+adj_matrix = nx.to_pandas_adjacency(grafo_normalizado).values
+
+# Converter a matriz de adjacência em um DataFrame do pandas
+df = pd.DataFrame(adj_matrix, columns=grafo_normalizado.nodes(), index=grafo_normalizado.nodes())
+
+# Criar um mapa de calor usando seaborn
+plt.figure(figsize=(12, 10))
+sns.heatmap(df, cmap='coolwarm', annot=True, fmt=".1f", cbar=False)
+plt.title('Mapa de Calor - Correlação entre Deputados')
+plt.xlabel('Deputados')
+plt.ylabel('Deputados')
+
+# Salvar o mapa de calor em um arquivo de imagem (opcional)
+plt.savefig('heatmap.png')
+
+# Exibir o mapa de calor
+plt.show()
+
+
+#-------------------------------------
+# Criar um layout de mola para o grafo_threshold
+layout = nx.spring_layout(grafo_threshold, seed=42)  # O 'seed' é opcional para reproduzibilidade
+
+# Plotar o grafo_threshold
+plt.figure(figsize=(12, 10))
+nx.draw(grafo_threshold, pos=layout, with_labels=True, node_size=50, node_color='skyblue', font_size=8, font_color='black', font_weight='bold', edge_color='gray', width=0.5)
+
+# Definir títulos e rótulos
+plt.title('Grafo de Relações de Votos Entre Deputados (com Threshold)')
+plt.xlabel('Deputados')
+plt.ylabel('Deputados')
+
+# Salvar o gráfico em um arquivo de imagem (por exemplo, formato PNG)
+plt.savefig('grafo_threshold.png')
+
+# Exibir o grafo_threshold
 plt.show()
